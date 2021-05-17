92b24d3c27f115e2560e89aa7e4120283e31d7e7
niikari <pakniiles@gmail.com>
2021-05-17 08:14:12 +0300

    viety verkkoon

--- src/database/Database.java
+++ src/database/Database.java
@@ -5,18 +5,19 @@ import java.sql.DriverManager;
 
 public class Database {
 	
-	private static final String URL = "jdbc:sqlite:C:\\Users\\pakni\\git\\niilespuuntiheys2\\balsa.sqlite";
+	//private static final String URL = "jdbc:sqlite:C:\\Users\\pakni\\git\\niilespuuntiheys2\\balsa.sqlite";
+	private static final String URL = "jdbc:sqlite:/home/niiles/balsa.sqlite";
+	
 	
 	public Connection connect() {
 		Connection connection = null;
 		try {
 			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager.getConnection(URL);
+			System.out.println(URL);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return connection;
 	}
-
 }
